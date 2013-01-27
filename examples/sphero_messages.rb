6b8357836cd94f8e2ff1125bf54293b621bc3c3c
deadprogrammer <ron.evans@gmail.com>
2013-01-26 18:17:12 -0800

    Display more update status when running example

--- examples/sphero_messages.rb
+++ examples/sphero_messages.rb
@@ -4,16 +4,18 @@ connection :sphero, :type => :sphero, :port => '/dev/tty.Sphero-BWY-RN-SPP'
 device :sphero
   
 work do
-	sphero.configure_collision_detection 0x01, 0x50, 0x50, 0x50, 0x50, 0x50
-	
+  puts "Configuring..."
+  sphero.configure_collision_detection 0x01, 0x50, 0x50, 0x50, 0x50, 0x50
+
   every(3.seconds) do
+    puts "Rolling..."
     sphero.roll 60, rand(360)
     if sphero.read_async_messages
-    	puts "----------"
-    	while m = sphero.async_responses.shift do
-    		puts m
-    	end
-    	puts "=========="
+      puts "----------"
+      while m = sphero.async_responses.shift do
+        puts m
+      end
+      puts "=========="
     end
   end
 end
