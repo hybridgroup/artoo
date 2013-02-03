73cb49952fd0610856b7d8f206aea3b6a7847b2b
deadprogrammer <ron.evans@gmail.com>
2013-02-02 18:55:34 -0800

    cleanup tabs

--- examples/sphero_firmata.rb
+++ examples/sphero_firmata.rb
@@ -7,18 +7,18 @@ connection :firmata, :type => :firmata, :port => '/dev/tty.usbserial-A700636n'
 device :firmata, :connection => :firmata
 
 work do
-	@counter = 0
+  @counter = 0
 
   every 3.seconds do
     sphero.roll 60, rand(360)
   end
 
   every 1.second do
-		firmata.digital_write(13, high_or_low)
+    firmata.digital_write(13, high_or_low)
     @counter += 1
   end
 end
 
 def high_or_low
-	@counter.even? ? Firmata::Board::HIGH : Firmata::Board::LOW
+  @counter.even? ? Firmata::Board::HIGH : Firmata::Board::LOW
 end
