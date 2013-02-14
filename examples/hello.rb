7d5705cf1d5fc82b6f961809b626bd234cd8bfa2
deadprogrammer <ron.evans@gmail.com>
2013-02-14 14:38:39 -0800

    Remove nonsense from example

--- examples/hello.rb
+++ examples/hello.rb
@@ -2,9 +2,6 @@ require 'artoo'
 
 connection :loop
 
-#device :collision_detect, :driver => :switch, :pin => 3
-#device :motor, :driver => :l293_motor, :pin => 4
-  
 work do
   every(3.seconds) do
      puts "hello"
