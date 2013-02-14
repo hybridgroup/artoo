f55bcb38a6942b515208045efbb990988cf3dc15
deadprogrammer <ron.evans@gmail.com>
2013-02-14 11:40:34 -0800

    Add correct firmata example with LED driver

--- examples/firmata.rb
+++ examples/firmata.rb
@@ -1,16 +1,17 @@
 require 'artoo'
 
 connection :firmata, :adaptor => :firmata, :port => '4567' #/dev/tty.usbserial-A700636n'
-device :firmata
+device :board
+device :led, :driver => :led, :pin => 13
 
 work do
-  puts "Firmware name #{firmata.firmware_name}"
-  puts "Firmata version #{firmata.version}"
-  10.times do
-    firmata.digital_write(13, Firmata::Board::HIGH)
-    firmata.delay 1
+  puts "Firmware name #{board.firmware_name}"
+  puts "Firmata version #{board.version}"
+  every 1.second do
+    led.on
+    board.delay 1
 
-    firmata.digital_write(13, Firmata::Board::LOW)
-    firmata.delay 1
+    led.off
+    board.delay 1
   end
 end
