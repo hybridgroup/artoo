228f0fadf62c038aafa0222953704e56fc98073d
deadprogrammer <ron.evans@gmail.com>
2013-02-14 12:31:04 -0800

    Supporting a few more LED related methods

--- lib/artoo/drivers/led.rb
+++ lib/artoo/drivers/led.rb
@@ -4,12 +4,33 @@ module Artoo
   module Drivers
     # The LED driver behaviors
     class Led < Driver
+      def is_on?
+        (@is_on ||= false) == true
+      end
+
+      def is_off?
+        (@is_on ||= false) == false
+      end
+
       def on
+        @is_on = true
+        connection.set_pin_mode(pin, Firmata::Board::OUTPUT)
         connection.digital_write(pin, Firmata::Board::HIGH)
       end
 
       def off
+        @is_on = false
+        connection.set_pin_mode(pin, Firmata::Board::OUTPUT)
         connection.digital_write(pin, Firmata::Board::LOW)
+      end
+
+      def toggle
+        is_off? ? on : off
+      end
+
+      def brightness(level=0)
+        connection.set_pin_mode(pin, Firmata::Board::PWM)
+        connection.analog_write(pin, level)
       end
     end
   end
--- test/drivers/led_test.rb
+++ test/drivers/led_test.rb
@@ -4,11 +4,49 @@ require 'artoo/drivers/led'
 describe Artoo::Drivers::Led do
   before do
     @device = mock('device')
-    @driver = Artoo::Drivers::Led.new(:parent => @device)
+    @pin = 13
+    @device.stubs(:pin).returns(@pin)
+    @led = Artoo::Drivers::Led.new(:parent => @device)
     @connection = mock('connection')
-    #@device.expects(:connection).returns(@connection)
+    @device.stubs(:connection).returns(@connection)
   end
 
-  it 'Led#on'
-  it 'Led#off'
+  it 'Led#is_on? default' do
+    @led.is_on?.must_equal false
+  end
+
+  it 'Led#is_off? default' do
+    @led.is_off?.must_equal true
+  end
+
+  it 'Led#on' do
+    @connection.expects(:set_pin_mode).with(@pin, Firmata::Board::OUTPUT)
+    @connection.expects(:digital_write).with(@pin, Firmata::Board::HIGH)
+    @led.on
+    @led.is_on?.must_equal true
+  end
+
+  it 'Led#off' do
+    @connection.expects(:set_pin_mode).with(@pin, Firmata::Board::OUTPUT)
+    @connection.expects(:digital_write).with(@pin, Firmata::Board::LOW)
+    @led.off
+    @led.is_off?.must_equal true
+  end
+
+  it 'Led#toggle' do
+    @connection.stubs(:set_pin_mode)
+    @connection.stubs(:digital_write)
+    @led.is_off?.must_equal true
+    @led.toggle
+    @led.is_on?.must_equal true
+    @led.toggle
+    @led.is_off?.must_equal true
+  end
+
+  it 'Led#brightness' do
+    val = 100
+    @connection.expects(:set_pin_mode).with(@pin, Firmata::Board::PWM)
+    @connection.expects(:analog_write).with(@pin, val)
+    @led.brightness(val)
+  end
 end
