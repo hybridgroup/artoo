07b17df10f0173856a895b0c7313baaf53207db8
deadprogrammer <ron.evans@gmail.com>
2013-02-16 19:56:43 -0800

    Even cleaner example now

--- examples/firmata_button.rb
+++ examples/firmata_button.rb
@@ -5,9 +5,5 @@ device :led, :driver => :led, :pin => 13
 device :button, :driver => :button, :pin => 2
 
 work do
-  on button, :push => :button_pushed
-end
-
-def button_pushed(*payload)
-  led.toggle
+  on button, :push => proc {led.toggle}
 end
