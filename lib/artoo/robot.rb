6a153126cfac0de9a2e6eb5300dab72c62ea1ea7
Ari Lerner <arilerner@mac.com>
2013-02-16 19:51:34 -0800

    Allow on to take a proc

--- lib/artoo/robot.rb
+++ lib/artoo/robot.rb
@@ -202,6 +202,30 @@ module Artoo
       end
     end
 
+    # Create an anonymous subscription method so we can wrap the
+    # subscription method fire into a valid method regardless
+    # of where it is defined
+    def create_proxy_method(k, v)
+      proxy_method_name(k).tap do |name|
+        self.class.send :define_method, name do |*args|
+          case v
+          when Symbol
+            self.send v.to_sym, *args
+          when Proc
+            v.call(*args)
+          end
+        end
+      end
+    end
+
+    # A simple loop to create a 'fake' anonymous method
+    def proxy_method_name(k)
+      begin
+        meth = "#{k}_#{Random.rand(999)}"
+      end while respond_to?(meth)
+      meth
+    end
+    
     private
 
     def initialize_connections(params={})
