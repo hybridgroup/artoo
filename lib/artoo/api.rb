c89e8203263d8c3d667f1d751e392d78b73f6793
deadprogrammer <ron.evans@gmail.com>
2013-02-18 23:33:27 -0800

    Experimental api server

--- lib/artoo/api.rb
+++ lib/artoo/api.rb
@@ -1,10 +1,29 @@
 require 'reel'
 
 module Artoo
-	class Api
-		include Reel::App
+	class Api < Reel::Server
+	  def initialize(host = "127.0.0.1", port = 3000)
+	    super(host, port, &method(:on_connection))
+	  end
 
-		get '/' { [200, {}, "hello world" }
+	  def on_connection(connection)
+	    while request = connection.request
+	      case request
+	      when Reel::Request
+	        handle_request(request)
+	      when Reel::WebSocket
+	        handle_websocket(request)
+	      end
+	    end
+	  end
+
+	  def handle_request(request)
+	    request.respond :ok, "Hello, world!"
+	  end
+
+	  def handle_websocket(sock)
+	    sock << "Hello everyone out there in WebSocket land!"
+	    sock.close
+	  end
 	end
 end
-
