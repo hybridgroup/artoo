8f687d786e014ed2dedb9687acc8f1c4ab3c931c
deadprogrammer <ron.evans@gmail.com>
2013-02-22 14:46:28 -0800

    Much improved Sphero stability

--- examples/sphero_color.rb
+++ examples/sphero_color.rb
@@ -4,14 +4,10 @@ connection :sphero, :adaptor => :sphero, :port => '4560' #/dev/tty.Sphero-YRR-RN
 device :sphero, :driver => :sphero
   
 work do
-	@count = 1
+  @count = 1
   every(3.seconds) do
-  	sphero.set_color(color)
+    sphero.set_color(@count % 2 == 0 ? :green : :red)
     @count += 1
     sphero.roll 60, rand(360)
   end
-end
-
-def color
-	@count % 2 == 0 ? :green : :red
 end
