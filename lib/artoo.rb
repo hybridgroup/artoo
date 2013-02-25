5452e69e587ab17926967e396bd4242a0553d7c4
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2013-02-25 13:42:19 -0800

    Removing duplicated code by just pointing to base

--- lib/artoo.rb
+++ lib/artoo.rb
@@ -1,12 +1,4 @@
-require 'celluloid/io'
-require 'multi_json'
-
-require 'artoo/utility'
-require 'artoo/robot'
-require 'artoo/connection'
-require 'artoo/device'
-require 'artoo/master'
-require 'artoo/port'
+require 'artoo/base'
 require 'artoo/delegator'
 
 module Artoo
