c25cca05824ddd38df28f06af0703c3255c5a553
deadprogrammer <ron.evans@gmail.com>
2013-02-27 18:39:16 -0800

    Starting to modularize

---
+++ lib/artoo/events.rb
@@ -0,0 +1,35 @@
+module Artoo
+  
+  module Events
+    # Subscribe to an event from a device
+    def on(device, events={})
+      events.each do |k, v|
+        subscribe("#{safe_name}_#{device.name}_#{k}", create_proxy_method(k, v))
+      end
+    end
+
+    # Create an anonymous subscription method so we can wrap the
+    # subscription method fire into a valid method regardless
+    # of where it is defined
+    def create_proxy_method(k, v)
+      proxy_method_name(k).tap do |name|
+        self.class.send :define_method, name do |*args|
+          case v
+          when Symbol
+            self.send v.to_sym, *args
+          when Proc
+            v.call(*args)
+          end
+        end
+      end
+    end
+
+    # A simple loop to create a 'fake' anonymous method
+    def proxy_method_name(k)
+      begin
+        meth = "#{k}_#{Random.rand(999)}"
+      end while respond_to?(meth)
+      meth
+    end
+  end
+end
--- lib/artoo/robot.rb
+++ lib/artoo/robot.rb
@@ -3,6 +3,7 @@ require 'multi_json'
 
 require 'artoo/connection'
 require 'artoo/device'
+require 'artoo/events'
 require 'artoo/api'
 require 'artoo/master'
 require 'artoo/port'
@@ -15,6 +16,7 @@ module Artoo
     include Celluloid
     include Celluloid::Notifications
     include Artoo::Utility
+    include Artoo::Events
 
     attr_reader :connections, :devices, :name
 
@@ -232,37 +234,6 @@ module Artoo
 
     def working_code
       current_class.working_code ||= proc {puts "No work defined."}
-    end
-    
-    # Subscribe to an event from a device
-    def on(device, events={})
-      events.each do |k, v|
-        subscribe("#{safe_name}_#{device.name}_#{k}", create_proxy_method(k, v))
-      end
-    end
-
-    # Create an anonymous subscription method so we can wrap the
-    # subscription method fire into a valid method regardless
-    # of where it is defined
-    def create_proxy_method(k, v)
-      proxy_method_name(k).tap do |name|
-        self.class.send :define_method, name do |*args|
-          case v
-          when Symbol
-            self.send v.to_sym, *args
-          when Proc
-            v.call(*args)
-          end
-        end
-      end
-    end
-
-    # A simple loop to create a 'fake' anonymous method
-    def proxy_method_name(k)
-      begin
-        meth = "#{k}_#{Random.rand(999)}"
-      end while respond_to?(meth)
-      meth
     end
     
     def to_hash
