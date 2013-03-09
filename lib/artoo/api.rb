d1ed9365cdad1c0df390c196b9ae167308bf0875
deadprogrammer <ron.evans@gmail.com>
2013-03-09 13:49:23 -0800

    Refactoring API server class

--- lib/artoo/api.rb
+++ lib/artoo/api.rb
@@ -21,28 +21,40 @@ module Artoo
     end
 
     get '/robots/:robotid' do
-      Actor[:master].get_robot_by_name(@params['robotid']).as_json
+      get_robot(@params['robotid']).as_json
     end
 
     get '/robots/:robotid/devices' do
-      MultiJson.dump(Actor[:master].get_robot_by_name(@params['robotid']).devices.each_value.collect {|d| d.to_hash})
+      MultiJson.dump(get_robot_devices(@params['robotid']).each_value.collect {|d| d.to_hash})
     end
 
     get '/robots/:robotid/devices/:deviceid' do
-      Actor[:master].get_robot_by_name(@params['robotid']).devices[@params['deviceid'].intern].as_json
+      get_robot_devices(@params['robotid'])[@params['deviceid'].intern].as_json
     end
 
     get_ws '/robots/:robotid/devices/:deviceid/events' do
-      DeviceEventClient.new(@req, Actor[:master].get_robot_by_name(@params['robotid']).devices[@params['deviceid'].intern].event_topic_name('update'))
+      DeviceEventClient.new(@req, get_robot_devices(@params['robotid'])[@params['deviceid'].intern].event_topic_name('update'))
       return nil
     end
 
     get '/robots/:robotid/connections' do
-      MultiJson.dump(Actor[:master].get_robot_by_name(@params['robotid']).connections.each_value.collect {|c| c.to_hash})
+      MultiJson.dump(get_robot_connections(@params['robotid']).each_value.collect {|c| c.to_hash})
     end
 
     get '/robots/:robotid/connections/:connectionid' do
-      Actor[:master].get_robot_by_name(@params['robotid']).connections[@params['connectionid'].intern].as_json
+      get_robot_connections(@params['robotid'])[@params['connectionid'].intern].as_json
+    end
+
+    def get_robot(robot_id)
+      Actor[:master].get_robot_by_name(robot_id)
+    end
+
+    def get_robot_devices(robot_id)
+      Actor[:master].get_robot_by_name(robot_id).devices
+    end
+
+    def get_robot_connections(robot_id)
+      Actor[:master].get_robot_by_name(robot_id).connections
     end
   end
 end
