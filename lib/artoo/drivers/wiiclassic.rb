e58ec7a3e8ddd363c301c4f17b2734bc43c2f46c
deadprogrammer <ron.evans@gmail.com>
2013-03-09 12:53:34 -0800

    More refactoring of Wiiclassic driver

--- lib/artoo/drivers/wiiclassic.rb
+++ lib/artoo/drivers/wiiclassic.rb
@@ -31,13 +31,17 @@ module Artoo
       end
 
       def update_buttons
-        publish(event_topic_name("a_button")) if data[:a] == true
-        publish(event_topic_name("b_button")) if data[:b] == true
-        publish(event_topic_name("x_button")) if data[:x] == true
-        publish(event_topic_name("y_button")) if data[:y] == true
-        publish(event_topic_name("home_button")) if data[:h] == true
-        publish(event_topic_name("start_button")) if data[:+] == true
-        publish(event_topic_name("select_button")) if data[:-] == true
+        update_button("a_button", :a)
+        update_button("b_button", :b)
+        update_button("x_button", :x)
+        update_button("y_button", :y)
+        update_button("home_button", :h)
+        update_button("start_button", :+)
+        update_button("select_button", :-)
+      end
+
+      def update_button(name, key)
+        publish(event_topic_name(name)) if data[key] == true
       end
 
       def update_left_joystick
@@ -74,20 +78,24 @@ module Artoo
           :ry => decode_value(value, 2) & 0x1f,
           :lt => ((decode_value(value, 2) & 0x60) >> 3) | ((decode_value(value, 3) & 0xC0) >> 6),
           :rt => decode_value(value, 3) & 0x1f,
-          :d_up => generate_bool(decode_value(value, 5)[0]),
-          :d_down => generate_bool(decode_value(value, 4)[6]),
-          :d_left => generate_bool(decode_value(value, 5)[1]),
-          :d_right => generate_bool(decode_value(value, 4)[7]),
-          :zr => generate_bool(decode_value(value, 5)[2]),
-          :zl => generate_bool(decode_value(value, 5)[7]),
-          :a => generate_bool(decode_value(value, 5)[4]),
-          :b => generate_bool(decode_value(value, 5)[6]),
-          :x => generate_bool(decode_value(value, 5)[3]),
-          :y => generate_bool(decode_value(value, 5)[5]),
-          :+ => generate_bool(decode_value(value, 4)[2]),
-          :- => generate_bool(decode_value(value, 4)[4]),
-          :h => generate_bool(decode_value(value, 4)[3]),
+          :d_up => get_bool_decoded_value(value, 5, 0),
+          :d_down => get_bool_decoded_value(value, 4, 6),
+          :d_left => get_bool_decoded_value(value, 5, 1),
+          :d_right => get_bool_decoded_value(value, 4, 7),
+          :zr => get_bool_decoded_value(value, 5, 2),
+          :zl => get_bool_decoded_value(value, 5, 7),
+          :a => get_bool_decoded_value(value, 5, 4),
+          :b => get_bool_decoded_value(value, 5, 6),
+          :x => get_bool_decoded_value(value, 5, 3),
+          :y => get_bool_decoded_value(value, 5, 5),
+          :+ => get_bool_decoded_value(value, 4, 2),
+          :- => get_bool_decoded_value(value, 4, 4),
+          :h => get_bool_decoded_value(value, 4, 3)
         }
+      end
+
+      def get_bool_decoded_value(value, offset1, offset2)
+        generate_bool(decode_value(value, offset1)[offset2])
       end
     end
   end
