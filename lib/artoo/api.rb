4afd08e64e7ede7ca35697e20a273e116c97ab95
deadprogrammer <ron.evans@gmail.com>
2013-03-09 15:40:00 -0800

    Refactor and add tests for Master

--- lib/artoo/api.rb
+++ lib/artoo/api.rb
@@ -21,48 +21,34 @@ module Artoo
     end
 
     get '/robots/:robotid' do
-      get_robot(@params['robotid']).as_json
+      master.get_robot(@params['robotid']).as_json
     end
 
     get '/robots/:robotid/devices' do
-      MultiJson.dump(get_robot_devices(@params['robotid']).each_value.collect {|d| d.to_hash})
+      MultiJson.dump(master.get_robot_devices(@params['robotid']).each_value.collect {|d| d.to_hash})
     end
 
     get '/robots/:robotid/devices/:deviceid' do
-      get_robot_devices(@params['robotid'])[@params['deviceid'].intern].as_json
+      master.get_robot_devices(@params['robotid'])[@params['deviceid'].intern].as_json
     end
 
     get_ws '/robots/:robotid/devices/:deviceid/events' do
-      DeviceEventClient.new(@req, get_robot_device(@params['robotid'], @params['deviceid']).event_topic_name('update'))
+      DeviceEventClient.new(@req, master.get_robot_device(@params['robotid'], @params['deviceid']).event_topic_name('update'))
       return nil
     end
 
     get '/robots/:robotid/connections' do
-      MultiJson.dump(get_robot_connections(@params['robotid']).each_value.collect {|c| c.to_hash})
+      MultiJson.dump(master.get_robot_connections(@params['robotid']).each_value.collect {|c| c.to_hash})
     end
 
     get '/robots/:robotid/connections/:connectionid' do
-      get_robot_connection(@params['robotid'], @params['connectionid']).as_json
+      master.get_robot_connection(@params['robotid'], @params['connectionid']).as_json
     end
 
-    def get_robot(robot_id)
-      Actor[:master].get_robot_by_name(robot_id)
-    end
+    protected
 
-    def get_robot_devices(robot_id)
-      get_robot(robot_id).devices
-    end
-
-    def get_robot_device(robot_id, device_id)
-      get_robot_devices(robot_id)[device_id.intern]
-    end
-
-    def get_robot_connections(robot_id)
-      get_robot(robot_id).connections
-    end
-
-    def get_robot_connection(robot_id, connection_id)
-      get_robot_connections(robot_id)[connection_id.intern]
+    def master
+      Actor[:master]
     end
   end
 end
---
+++ test/master_test.rb
@@ -0,0 +1,45 @@
+require File.expand_path(File.dirname(__FILE__) + "/test_helper")
+
+class MockRobot
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def devices
+    ["#{name}-device1", "#{name}-device2", "#{name}-device3"]
+  end
+
+  def connections
+    ["#{name}-connection1", "#{name}-connection2", "#{name}-connection3"]
+  end
+end
+
+describe Artoo::Master do
+  before do
+    @robots = []
+
+    @robot1 = MockRobot.new("robot1")
+    @robot2 = MockRobot.new("robot2")
+    @robot3 = MockRobot.new("robot3")
+    
+    @robots << @robot1
+    @robots << @robot2
+    @robots << @robot3
+
+    @master = Artoo::Master.new(@robots)
+  end
+
+  it 'Artoo::Master#get_robot' do
+    @master.get_robot("robot2").must_equal @robot2
+  end
+
+  it 'Artoo::Master#get_robot_devices' do
+    @master.get_robot_devices("robot2").first.must_equal "robot2-device1"
+  end
+
+  it 'Artoo::Master#get_robot_connections' do
+    @master.get_robot_connections("robot2").last.must_equal "robot2-connection3"
+  end
+end
