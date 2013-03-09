96a3419f5d55965b03c59370ee0161150c138d24
deadprogrammer <ron.evans@gmail.com>
2013-03-09 15:40:00 -0800

    Refactor and add tests for Master

--- lib/artoo/master.rb
+++ lib/artoo/master.rb
@@ -9,8 +9,24 @@ module Artoo
       @robots = bots
     end
 
-    def get_robot_by_name(name)
+    def get_robot(name)
     	robots.find_all {|r| r.name == name}.first
+    end
+
+    def get_robot_devices(name)
+      get_robot(name).devices
+    end
+
+    def get_robot_device(name, device_id)
+      get_robot_devices(name)[device_id.intern]
+    end
+
+    def get_robot_connections(name)
+      get_robot(name).connections
+    end
+
+    def get_robot_connection(robot_id, connection_id)
+      get_robot_connections(robot_id)[connection_id.intern]
     end
   end
 end
