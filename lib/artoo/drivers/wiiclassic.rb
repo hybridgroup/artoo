9bff013c9e485157e607d365d226fe5abf98ba5b
deadprogrammer <ron.evans@gmail.com>
2013-03-08 21:41:36 -0800

    Moving to shared class for common Wii-based controller functionality

--- lib/artoo/drivers/wiiclassic.rb
+++ lib/artoo/drivers/wiiclassic.rb
@@ -1,58 +1,12 @@
-require 'artoo/drivers/driver'
+require 'artoo/drivers/wiidriver'
 
 module Artoo
   module Drivers
     # Wiiclassic driver behaviors for Firmata
-    class Wiiclassic < Driver
-      attr_reader :joystick, :data
-
-      def address; 0x52; end
-
-      INITIAL_DEFAULTS = {
-        :ry_origin => nil,
-        :rx_origin => nil,
-        :ly_origin => nil,
-        :lx_origin => nil,
-        :rt_origin => nil,
-        :lt_origin => nil
-      }  
-
-      def initialize(params={})
-        @joystick = INITIAL_DEFAULTS
-        super
-      end
-
-      def start_driver
-        begin
-        listener = ->(value) { update(value) }
-        connection.on("i2c_reply", listener)
-
-        connection.i2c_config(0)
-        every(interval) do
-          connection.i2c_write_request(address, 0x40, 0x00)
-          p
-          connection.i2c_write_request(address, 0x00, 0x00)
-          p
-          connection.i2c_read_request(address, 6)
-          p
-          connection.read_and_process
-        end
-        
-        super
-        rescue Exception => e
-          p "start driver"
-          p e.message
-          p e.backtrace.inspect
-        end
-
-      end
-
+    class Wiiclassic < Wiidriver      
       def update(value)
         begin
-          if encrypted?(value)
-            Logger.error "Encrypted bytes from wiiclassic!"
-            return
-          end
+          super
 
           @data = parse_wiiclassic(value)
           
@@ -78,10 +32,6 @@ module Artoo
         set_joystick_default_value(:lt_origin, data[:lt])
       end
 
-      def set_joystick_default_value(joystick_axis, default_value)     
-        joystick[joystick_axis] = default_value if joystick[joystick_axis].nil?
-      end
-
       def update_buttons
         publish(event_topic_name("a_button")) if data[:a] == true
         publish(event_topic_name("b_button")) if data[:b] == true
@@ -105,27 +55,18 @@ module Artoo
         publish(event_topic_name("left_trigger"), calculate_joystick_value(:lt, :lt_origin))
       end
 
-      private 
-
-      def calculate_joystick_value(axis, origin)
-        data[axis] - joystick[origin]
-      end
-
-      def encrypted?(value)
-        [[0, 1], [2, 3], [4, 5]].all? {|a| get_value(value, a[0]) == get_value(value, a[1]) }
-      end
-
-      def decode(x)
-        return ( x ^ 0x17 ) + 0x17
-      end
-
-      def decode_value(value, index)
-        decode(get_value(value, index))
-      end
-
-      def get_value(value, index)
-        value[:data][index]
-      end
+      private
+      
+      def get_defaults
+        {
+          :ry_origin => nil,
+          :rx_origin => nil,
+          :ly_origin => nil,
+          :lx_origin => nil,
+          :rt_origin => nil,
+          :lt_origin => nil
+        }  
+      end      
 
       def parse_wiiclassic(value)
         return {
@@ -149,10 +90,6 @@ module Artoo
           :- => generate_bool(decode_value(value, 4)[4]),
           :h => generate_bool(decode_value(value, 4)[3]),
         }
-      end
-
-      def generate_bool(value)
-        value == 0 ? true : false
       end
     end
   end
---
+++ lib/artoo/drivers/wiidriver.rb
@@ -0,0 +1,83 @@
+require 'artoo/drivers/driver'
+
+module Artoo
+  module Drivers
+    # Wii-based controller shared driver behaviors for Firmata
+    class Wiidriver < Driver
+      attr_reader :joystick, :data
+
+      def address; 0x52; end
+
+      def initialize(params={})
+        @joystick = get_defaults
+        super
+      end
+
+      def start_driver
+        begin
+        listener = ->(value) { update(value) }
+        connection.on("i2c_reply", listener)
+
+        connection.i2c_config(0)
+        every(interval) do
+          connection.i2c_write_request(address, 0x40, 0x00)
+          p
+          connection.i2c_write_request(address, 0x00, 0x00)
+          p
+          connection.i2c_read_request(address, 6)
+          p
+          connection.read_and_process
+        end
+        
+        super
+        rescue Exception => e
+          p "start driver"
+          p e.message
+          p e.backtrace.inspect
+        end
+
+      end
+
+      def update(value)
+        if encrypted?(value)
+          Logger.error "Encrypted bytes from wii device!"
+          raise "Encrypted bytes from wii device!"
+        end
+      end
+
+      def set_joystick_default_value(joystick_axis, default_value)     
+        joystick[joystick_axis] = default_value if joystick[joystick_axis].nil?
+      end
+
+      private 
+
+      def get_defaults
+        {}
+      end
+
+      def calculate_joystick_value(axis, origin)
+        data[axis] - joystick[origin]
+      end
+
+      def encrypted?(value)
+        [[0, 1], [2, 3], [4, 5]].all? {|a| get_value(value, a[0]) == get_value(value, a[1]) }
+      end
+
+      def decode(x)
+        return ( x ^ 0x17 ) + 0x17
+      end
+
+      def decode_value(value, index)
+        decode(get_value(value, index))
+      end
+
+      def get_value(value, index)
+        value[:data][index]
+      end
+
+      def generate_bool(value)
+        value == 0 ? true : false
+      end
+    end
+  end
+end
