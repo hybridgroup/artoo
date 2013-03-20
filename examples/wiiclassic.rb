5018314ef79a35237513179739e3b5399b782ccd
Adrian Zankich <adrian@hybridgroup.com>
2013-03-20 16:32:31 -0700

    Tighten up output in wiiclassic example

--- examples/wiiclassic.rb
+++ examples/wiiclassic.rb
@@ -12,15 +12,15 @@ work do
   on classic, :start_button => proc { puts "start button pressed!" }
   on classic, :select_button => proc { puts "select button pressed!" }
   on classic, :left_joystick => proc { |*value|
-    puts "left joystick x: #{value[1][:x]}, y: #{value[1][:y]}"
+    puts "left joystick x: #{value[1][:x]}, y: #{value[1][:y]}" unless (value[1][:x] == 0 && value[1][:y] == 0)
   }
   on classic, :right_joystick => proc { |*value|
-    puts "right joystick x: #{value[1][:x]}, y: #{value[1][:y]}"
+    puts "right joystick x: #{value[1][:x]}, y: #{value[1][:y]}" unless (value[1][:x] == 0 && value[1][:y] == 0)
   }
   on classic, :right_trigger => proc { |*value|
-    puts "right trigger: #{value[1]}"
+    puts "right trigger: #{value[1]}" unless (value[1]== 0)
   }
   on classic, :left_trigger => proc { |*value|
-    puts "left trigger: #{value[1]}"
+    puts "left trigger: #{value[1]}" unless (value[1]== 0)
   }
 end
