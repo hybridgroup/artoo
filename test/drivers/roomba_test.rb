bbaa7b5ee66173c6f5023aa888b8587f6c0dd728
deadprogrammer <ron@hybridgroup.com>
2013-04-12 20:20:33 -0700

    Finish refactring and test coverage for the original Roomba functionality

--- test/drivers/roomba_test.rb
+++ test/drivers/roomba_test.rb
@@ -8,6 +8,7 @@ describe Artoo::Drivers::Roomba do
     @device.stubs(:connection).returns(@connection)
     @roomba = Artoo::Drivers::Roomba.new(:parent => @device)
     @roomba.stubs(:sleep)
+    @roomba_proxy = @roomba.wrapped_object
   end
 
   it 'Roomba#start' do
@@ -28,49 +29,59 @@ describe Artoo::Drivers::Roomba do
   end
 
   it 'Roomba#forward' do
-    @roomba.wrapped_object.expects(:drive).with(250, Artoo::Drivers::Roomba::Direction::STRAIGHT, 10)
-    @roomba.wrapped_object.expects(:stop)
+    @roomba_proxy.expects(:drive).with(250, Artoo::Drivers::Roomba::Direction::STRAIGHT, 10)
+    @roomba_proxy.expects(:stop)
     @roomba.forward(10, 250)
   end
 
   it 'Roomba#stop' do
-    @roomba.wrapped_object.expects(:drive).with(Artoo::Drivers::Roomba::Speed::ZERO, Artoo::Drivers::Roomba::Direction::STRAIGHT)
+    @roomba_proxy.expects(:drive).with(Artoo::Drivers::Roomba::Speed::ZERO, Artoo::Drivers::Roomba::Direction::STRAIGHT)
     @roomba.stop
   end
 
   it 'Roomba#fast_forward' do
-    @roomba.wrapped_object.expects(:drive).with(Artoo::Drivers::Roomba::Speed::MAX, Artoo::Drivers::Roomba::Direction::STRAIGHT, 10)
-    @roomba.wrapped_object.expects(:stop)
+    @roomba_proxy.expects(:drive).with(Artoo::Drivers::Roomba::Speed::MAX, Artoo::Drivers::Roomba::Direction::STRAIGHT, 10)
+    @roomba_proxy.expects(:stop)
     @roomba.fast_forward(10)
   end
 
   it 'Roomba#backwards' do
-    @roomba.wrapped_object.expects(:drive).with(Artoo::Drivers::Roomba::Speed::NEG, Artoo::Drivers::Roomba::Direction::STRAIGHT, 10)
-    @roomba.wrapped_object.expects(:stop)
+    @roomba_proxy.expects(:drive).with(Artoo::Drivers::Roomba::Speed::NEG, Artoo::Drivers::Roomba::Direction::STRAIGHT, 10)
+    @roomba_proxy.expects(:stop)
     @roomba.backwards(10)
   end
 
   it 'Roomba#turn_left' do
-    @roomba.wrapped_object.expects(:drive).with(Artoo::Drivers::Roomba::Speed::SLOW, Artoo::Drivers::Roomba::Direction::COUNTERCLOCKWISE, 10)
-    @roomba.wrapped_object.expects(:stop)
+    @roomba_proxy.expects(:drive).with(Artoo::Drivers::Roomba::Speed::SLOW, Artoo::Drivers::Roomba::Direction::COUNTERCLOCKWISE, 10)
+    @roomba_proxy.expects(:stop)
     @roomba.turn_left(10)
   end
 
   it 'Roomba#nudge_left' do
-    @roomba.wrapped_object.expects(:drive).with(Artoo::Drivers::Roomba::Speed::SLOW, Artoo::Drivers::Roomba::Direction::COUNTERCLOCKWISE, 0.25)
-    @roomba.wrapped_object.expects(:stop)
+    @roomba_proxy.expects(:drive).with(Artoo::Drivers::Roomba::Speed::SLOW, Artoo::Drivers::Roomba::Direction::COUNTERCLOCKWISE, 0.25)
+    @roomba_proxy.expects(:stop)
     @roomba.nudge_left
   end
 
   it 'Roomba#turn_right' do
-    @roomba.wrapped_object.expects(:drive).with(Artoo::Drivers::Roomba::Speed::SLOW, Artoo::Drivers::Roomba::Direction::CLOCKWISE, 10)
-    @roomba.wrapped_object.expects(:stop)
+    @roomba_proxy.expects(:drive).with(Artoo::Drivers::Roomba::Speed::SLOW, Artoo::Drivers::Roomba::Direction::CLOCKWISE, 10)
+    @roomba_proxy.expects(:stop)
     @roomba.turn_right(10)
   end
 
   it 'Roomba#nudge_right' do
-    @roomba.wrapped_object.expects(:drive).with(Artoo::Drivers::Roomba::Speed::SLOW, Artoo::Drivers::Roomba::Direction::CLOCKWISE, 0.25)
-    @roomba.wrapped_object.expects(:stop)
+    @roomba_proxy.expects(:drive).with(Artoo::Drivers::Roomba::Speed::SLOW, Artoo::Drivers::Roomba::Direction::CLOCKWISE, 0.25)
+    @roomba_proxy.expects(:stop)
     @roomba.nudge_right
+  end
+
+  it 'Roomba#play' do
+    @connection.expects(:send_bytes).with([Artoo::Drivers::Roomba::Song::PLAY, 0])
+    @roomba.play(0)
+  end
+
+  it 'Roomba#song' do
+    @connection.expects(:send_bytes).with([Artoo::Drivers::Roomba::Song::SONG, 0, 3, Artoo::Drivers::Roomba::Note::B, Artoo::Drivers::Roomba::Note::QUARTER, Artoo::Drivers::Roomba::Note::B, Artoo::Drivers::Roomba::Note::QUARTER, Artoo::Drivers::Roomba::Note::B, Artoo::Drivers::Roomba::Note::HALF])
+    @roomba.song([[Artoo::Drivers::Roomba::Note::B, Artoo::Drivers::Roomba::Note::QUARTER], [Artoo::Drivers::Roomba::Note::B, Artoo::Drivers::Roomba::Note::QUARTER], [Artoo::Drivers::Roomba::Note::B, Artoo::Drivers::Roomba::Note::HALF]], 0)
   end
 end
