65a6f1d118be75307aee06f3c5048440b6c6fb79
deadprogrammer <ron@hybridgroup.com>
2013-05-08 22:27:15 -0700

    Correct some errors in current API and MCP

--- lib/artoo/api.rb
+++ lib/artoo/api.rb
@@ -21,15 +21,19 @@ module Artoo
     end
 
     get '/robots/:robotid' do
-      master.get_robot(@params['robotid']).as_json
+      master.robot(@params['robotid']).as_json
     end
 
     get '/robots/:robotid/devices' do
-      MultiJson.dump(master.get_robot_devices(@params['robotid']).each_value.collect {|d| d.to_hash})
+      MultiJson.dump(master.robot_devices(@params['robotid']).each_value.collect {|d| d.to_hash})
     end
 
     get '/robots/:robotid/devices/:deviceid' do
       device(@params['robotid'], @params['deviceid']).as_json
+    end
+
+    get '/robots/:robotid/devices/:deviceid/commands' do
+      MultiJson.dump(device(@params['robotid'], @params['deviceid']).commands)
     end
 
     get_ws '/robots/:robotid/devices/:deviceid/events' do
@@ -38,11 +42,11 @@ module Artoo
     end
 
     get '/robots/:robotid/connections' do
-      MultiJson.dump(master.get_robot_connections(@params['robotid']).each_value.collect {|c| c.to_hash})
+      MultiJson.dump(master.robot_connections(@params['robotid']).each_value.collect {|c| c.to_hash})
     end
 
     get '/robots/:robotid/connections/:connectionid' do
-      master.get_robot_connection(@params['robotid'], @params['connectionid']).as_json
+      master.robot_connection(@params['robotid'], @params['connectionid']).as_json
     end
 
     protected
@@ -52,7 +56,7 @@ module Artoo
     end
 
     def device(robot_id, device_id)
-      master.get_robot_device(robot_id, device_id)
+      master.robot_device(robot_id, device_id)
     end
   end
 end
