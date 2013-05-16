e7e7633b60ea988112e49ebccc3c63b26dcd370d
Adrian Zankich <adrian@hybridgroup.com>
2013-05-15 17:23:11 -0700

    Add optional bluetooth hci parameter to sphero_linux_bind.sh

--- bin/sphero_linux_bind.sh
+++ bin/sphero_linux_bind.sh
@@ -7,11 +7,19 @@
 # $1 = unbound /dev/rfcommXX
 # $2 = sphero hardware address from hcitool scan
 # $3 = sphero three letter color code
+#
+# Optional parameter
+# $4 = bluetooth radio hcix
 # 
 # Example
 #
-# sudo ./sphero_linux_bind.sh 1 00:06:66:4A:43:23 PYG
+# sudo ./sphero_linux_bind.sh 1 00:06:66:4A:43:23 PYG hci1
 #
 #
-rfcomm bind /dev/rfcomm$1 $2 1
+if [ -z "$4" ]; then
+  addr="hci0"
+else
+  addr=$4
+fi
+rfcomm -i $addr bind /dev/rfcomm$1 $2 1
 ln -s /dev/rfcomm$1 /dev/Sphero-$3
