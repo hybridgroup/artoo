91f48c55d840722c51dd4d96b263df07bfb4031e
deadprogrammer <ron@hybridgroup.com>
2013-05-19 14:47:54 -0700

    Corrections needed for Sphero example

--- examples/sphero_messages.rb
+++ examples/sphero_messages.rb
@@ -3,11 +3,13 @@ require 'artoo'
 connection :sphero, :adaptor => :sphero, :port => '127.0.0.1:4560'
 device :sphero, :driver => :sphero
 
-def contact; @contacts += 1; end
+def contact(*args)
+  @contacts ||= 0
+  @contacts += 1
+  puts "Contact #{@contacts}"
+end
 
 work do
-  @contacts = 0
-
   on sphero, :collision => :contact
 
   every(3.seconds) do
