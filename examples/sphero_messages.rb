51adcfafd5d777797dfaea3c6508703ff18a08ca
deadprogrammer <ron@hybridgroup.com>
2013-05-18 19:12:06 -0700

    Update example to match the test example

--- examples/sphero_messages.rb
+++ examples/sphero_messages.rb
@@ -2,9 +2,13 @@ require 'artoo'
 
 connection :sphero, :adaptor => :sphero, :port => '127.0.0.1:4560'
 device :sphero, :driver => :sphero
-  
+
+def contact; @contacts += 1; end
+
 work do
-  on sphero, :collision => proc { puts "Collision!" }
+  @contacts = 0
+
+  on sphero, :collision => :contact
 
   every(3.seconds) do
     puts "Rolling..."
