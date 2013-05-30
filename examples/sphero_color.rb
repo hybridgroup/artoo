9c8370017436cb764da5f1f9407327b7859f3166
Adrian Zankich <adrian@hybridgroup.com>
2013-05-29 18:54:44 -0700

    Update sphero examples for rubykaigi

--- examples/sphero_color.rb
+++ examples/sphero_color.rb
@@ -3,28 +3,11 @@ require 'artoo'
 connection :sphero, :adaptor => :sphero, :port => '127.0.0.1:4569'
 device :sphero, :driver => :sphero
 
-connection :arduino, :adaptor => :firmata, :port => "8023"
-device :wiichuck, :driver => :wiichuck, :connection => :arduino, :interval => 0.1
-  
 work do
-  init_settings
-
-  on wiichuck, :joystick => proc { |*value|
-    @heading = heading(value[1])
-  }
-
   every(1.seconds) do
-  	puts "Rolling..."
+    puts "Rolling..."
     sphero.set_color(rand(255),rand(255),rand(255))
-    sphero.roll 20, @heading
+    sphero.roll 20, rand(360)
   end
 end
 
-def init_settings
-  sphero.stop
-  @heading = 0
-end
-
-def heading(value)
-  (180.0 - (Math.atan2(value[:y],value[:x]) * (180.0 / Math::PI))).round
-end
