50fd83aba91ae73be2632ac24b1ef98f2e43c298
deadprogrammer <ron@hybridgroup.com>
2013-06-25 15:36:57 -0700

    Adaptor generator appears to be fully functional

--- lib/artoo/generators/adaptor.rb
+++ lib/artoo/generators/adaptor.rb
@@ -1,12 +1,26 @@
 require 'thor'
 require 'thor/group'
+require 'artoo/version'
 
 module Artoo
   module Generator
     class Adaptor < Thor::Group
       include Thor::Actions
+      include Artoo::Utility
 
       argument :adaptor_name
+
+      def self.source_root
+        File.dirname(__FILE__)
+      end
+
+      def adaptor_class_name
+        classify(adaptor_name)
+      end
+
+      def artoo_version
+        Artoo::VERSION
+      end
 
       def copy_adaptor_directory
         say "Creating #{adaptor_name} adaptor..."
