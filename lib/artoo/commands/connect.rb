a1c47588bdbb6aec931b8f312fe31a3b615742e7
deadprogrammer <ron@hybridgroup.com>
2013-07-24 09:47:52 -0700

    Only run connect commands on supported OSes

--- lib/artoo/commands/connect.rb
+++ lib/artoo/commands/connect.rb
@@ -5,20 +5,33 @@ module Artoo
   module Commands
     class Connect < Thor
       include Thor::Actions
-
+      include Artoo::Utility
+      
       desc "scan", "scan for connected devices"
       def scan
-        run("hcitool scan")
+        if os == :linux
+          run("hcitool scan")
+        else
+          say "OS not yet supported..."
+        end
       end
 
       desc "bind", "bind [COMM] [ADDRESS] [NAME] binds a device to some connected hardware"
       def bind(comm, address, name, hcix=nil)
-        run("bundle exec ./bin/artoo_bind.sh #{comm} #{address} #{name}")
+        if os == :linux
+          run("bundle exec ./bin/artoo_bind.sh #{comm} #{address} #{name}")
+        else
+          say "OS not yet supported..."
+        end
       end
 
       desc "socat", "socat [PORT] [NAME] use socat to connect a socket to a device by name"
       def socat(port, name)
-        run("bundle exec ./bin/artoo_socat.sh #{port} #{name}")
+        if os == :linux
+          run("bundle exec ./bin/artoo_socat.sh #{port} #{name}")
+        else
+          say "OS not yet supported..."
+        end
       end
     end
   end
