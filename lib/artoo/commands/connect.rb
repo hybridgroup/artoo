b701ca40e762e3a8e51742d7bf22d327081da73b
deadprogrammer <ron@hybridgroup.com>
2013-07-23 22:32:34 -0700

    Small tweaks to command descriptions

--- lib/artoo/commands/connect.rb
+++ lib/artoo/commands/connect.rb
@@ -11,12 +11,12 @@ module Artoo
         run("hcitool scan")
       end
 
-      desc "bind", "bind [COMM] [ADDRESS] [NAME] binds a port to a connected device"
+      desc "bind", "bind [COMM] [ADDRESS] [NAME] binds a device to some connected hardware"
       def bind(comm, address, name, hcix=nil)
         run("bundle exec ./bin/artoo_bind.sh #{comm} #{address} #{name}")
       end
 
-      desc "socat", "socat [PORT] [NAME] use socat to connect a socket to a port"
+      desc "socat", "socat [PORT] [NAME] use socat to connect a socket to a device by name"
       def socat(port, name)
         run("bundle exec ./bin/artoo_socat.sh #{port} #{name}")
       end
