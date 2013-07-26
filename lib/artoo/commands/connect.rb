031c1bc5019eea97bca334c897b049398a617cc7
deadprogram <ron@hybridgroup.com>
2013-07-26 13:54:56 -0700

    Everything in bin is Ruby now...

--- lib/artoo/commands/connect.rb
+++ lib/artoo/commands/connect.rb
@@ -21,10 +21,12 @@ module Artoo
 
       desc "bind", "bind [ADDRESS] [NAME] binds a device to some connected hardware"
       option :comm, :default => 0, :desc => "Comm number"
-      def bind(address, name, hcix=nil)
+      option :radio, :default => "hci0", :desc => "Bluetooth radio address"
+      def bind(address, name)
         case os
         when :linux
-          run("bundle exec ./bin/artoo_bind.sh #{options[:comm]} #{address} #{name}")
+          run("rfcomm -i #{options[:radio]} bind /dev/rfcomm#{options[:comm]} #{address} 1")
+          run("sudo ln -s /dev/rfcomm#{options[:comm]} /dev/#{name}")
         when :macosx
           say "OSX binds devices on its own volition."
         else
@@ -33,12 +35,25 @@ module Artoo
       end
 
       desc "socat", "socat [PORT] [NAME] use socat to connect a socket to a device by name"
+      option :attempts, :default => 1, :desc => "Number of times to attempt to connect"
       def socat(port, name)
+        attempts = options[:attempts].to_i
+        
         case os
         when :linux
-          run("bundle exec ./bin/artoo_socat_linux.sh #{port} #{name}")
+          while(attempts > 0) do
+            run("socat -d -d FILE:/dev/#{name},nonblock,raw,b115200,echo=0 TCP-LISTEN:#{port},fork")
+            break unless $? == 1
+            attempts -= 1
+          end
+
         when :macosx
-          run("bundle exec ./bin/artoo_socat_osx.sh #{port} #{name}")          
+          while(attempts > 0) do
+            run("socat -d -d FILE:/dev/#{name},nonblock,raw,echo=0 TCP-LISTEN:#{port},fork")
+            break unless $? == 1
+            attempts -= 1
+          end
+
         else
           say "OS not yet supported..."
         end
