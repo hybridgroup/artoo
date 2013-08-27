f2c8bc5a99f0c352ed80b6caa96f3605aefd5784
deadprogram <ron@hybridgroup.com>
2013-08-27 14:50:37 -0700

    Set the level for logging to INFO or above

--- lib/artoo/robot.rb
+++ lib/artoo/robot.rb
@@ -31,6 +31,8 @@ module Artoo
 
     attr_reader :connections, :devices, :name
 
+    Celluloid.logger.level = ::Logger::Severity::INFO
+
     # Create new robot
     # @param [Hash] params
     # @option params [String]     :name
