61a3057d95c92fac7b29ed0fc0a4ac925cbee82e
deadprogram <ron@hybridgroup.com>
2013-11-04 22:46:23 -0800

    Revert "Remove moneypatched timer code, since is now in timer gem itself"
    This reverts commit 7dbd4a0b284baa50104b383374f5e4aee4f6efb6.

--- lib/artoo/robot.rb
+++ lib/artoo/robot.rb
@@ -1,8 +1,9 @@
 require 'celluloid/autostart'
 require 'celluloid/io'
 require 'multi_json'
-
+require 'artoo/ext/timers'
 require 'artoo/ext/actor'
+
 require 'artoo/robot_class_methods'
 require 'artoo/basic'
 require 'artoo/connection'
