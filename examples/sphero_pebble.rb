7933caf61365f9643a607ecc3ea4b20067670755
Javier Cervantes <1.27201@gmail.com>
2014-04-21 12:29:01 -0500

    Adding example of sphero controlled by pebble

---
+++ examples/sphero_pebble.rb
@@ -0,0 +1,42 @@
+require 'artoo'
+
+connection :sphero, :adaptor => :sphero, :port => '127.0.0.1:1234'
+device     :sphero, :driver  => :sphero
+
+connection :pebble, :adaptor => :pebble
+device     :watch,  :driver  => :pebble, :name => 'pebble'
+
+api :host => '0.0.0.0', :port => '8080'
+
+name 'pebble'
+
+def move_forward
+  p 'moving forward'
+  sphero.roll 100, 0
+  sleep 4
+  sphero.stop
+end
+
+def move_backward
+  p 'moving backward'
+  sphero.roll 100, 180
+  sleep 4
+  sphero.stop
+end
+
+def button_push(*data)
+  unless data[1].nil?
+    case data[1]
+    when 'up' then
+      move_forward
+    when 'select' then
+      sphero.set_color(rand(255),rand(255),rand(255))
+    when 'down' then
+      move_backward
+    end
+  end
+end
+
+work do
+  on pebble, :button => :button_push
+end
