40b8ef7c75cedd13ca11b74c6ed5c27280116300
xixebombilla <tiliches88@gmail.com>
2014-04-28 17:33:44 -0500

    base side documentation

---
+++ source/hybridpages/assets/javascript/accordeon.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    var allPanels = $('.accordion-docs > dd').hide();
+    
+  $('.accordion-docs > dt > a').click(function() {
+    allPanels.slideUp();
+    $(this).parent().next().slideDown();
+    return false;
+  });
+
+});
